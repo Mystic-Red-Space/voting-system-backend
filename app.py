6d32d837c797defba85c18cfe734f3da58c1a5b0
Coder-Iro <Coder-Iro@teamif.io>
2021-01-28 00:48:31 +0900

    Add remote interpreter option

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 
+load_dotenv(override=True)
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/voting"
+mongo = PyMongo(app)
 
 
 @app.route('/')
